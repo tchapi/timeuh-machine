37263b0e0ceea965038c19c9667b2191901da07b
Cyril <cyril@joshfire.com>
2019-02-13 14:59:13 +0100

    Gain 20x on SQL queries, thanks M. Winand

--- procedures.sql
+++ procedures.sql
@@ -53,25 +53,18 @@ CREATE PROCEDURE refresh_months_mv_now (
 )
 BEGIN
 
-  DECLARE current_month INT;
-  DECLARE selects LONGTEXT;
-
-  SET selects =  '';
-  SET current_month = 1;
- 
-  WHILE current_month <= 12 DO
-      SET selects = CONCAT(selects, '(SELECT t.title, t.album, t.artist, t.image, ', current_year, ' as year_n, ', current_month, ' as month_n FROM track AS t WHERE t.valid = 1 AND t.image != \'\' AND YEAR(t.started_at) = ', current_year, ' AND MONTH(t.started_at) = ', current_month, ' LIMIT 16)');
-
-      IF current_month < 12 THEN 
-        SET selects = CONCAT(selects, ' UNION ALL ');
-      END  IF;
-      SET current_month = current_month + 1; 
-  END WHILE;
-
-  SET selects = CONCAT('INSERT INTO `months_mv` (`title`, `album`, `artist`, `image`, `year_n`, `month_n`) ', selects);
   DELETE FROM `months_mv` WHERE `year_n` = current_year;
-  PREPARE stmt FROM selects;
-  EXECUTE stmt;
+  INSERT INTO `months_mv` (`title`, `album`, `artist`, `image`, `year_n`, `month_n`)
+    SELECT `title`, `album`, `artist`, `image`, `year_n`, `month_n`
+    FROM (
+      SELECT *, YEAR(`started_at`) AS year_n, MONTH(`started_at`) AS month_n,
+       ROW_NUMBER() OVER(PARTITION BY year_n, month_n) AS rank
+      FROM `track`
+      WHERE valid = 1 AND image != ''
+      HAVING year_n = current_year
+    ) ranked
+    WHERE rank <= 16
+  ;
 
 END;
 $$
@@ -101,25 +94,16 @@ CREATE PROCEDURE refresh_days_mv_now (
 )
 BEGIN
 
-  DECLARE current_day INT;
-  DECLARE selects LONGTEXT;
-
-  SET selects =  '';
-  SET current_day = 1;
-
-  WHILE current_day <= 31 DO
-      SET selects = CONCAT(selects, '(SELECT t.title, t.album, t.artist, t.image, ', current_year, ' as year_n, ', current_month, ' as month_n, ', current_day, ' as day_n FROM track AS t WHERE t.valid = 1 AND t.image != \'\' AND YEAR(t.started_at) = ', current_year, ' AND MONTH(t.started_at) = ', current_month, ' AND DAY(t.started_at) = ', current_day, ' LIMIT 8)');
-
-      IF current_day < 31 THEN 
-        SET selects = CONCAT(selects, ' UNION ALL ');
-      END IF;
-      SET current_day = current_day + 1; 
-  END WHILE;
-
-  SET selects = CONCAT('INSERT INTO `days_mv` (`title`, `album`, `artist`, `image`, `year_n`, `month_n`, `day_n`) ', selects);
   DELETE FROM `days_mv` WHERE `year_n` = current_year AND `month_n` = current_month;
-  PREPARE stmt FROM selects;
-  EXECUTE stmt;
+  INSERT INTO `days_mv` (`title`, `album`, `artist`, `image`, `year_n`, `month_n`, `day_n`)
+    SELECT `title`, `album`, `artist`, `image`, `year_n`, `month_n`, `day_n`
+    FROM (
+      SELECT *, YEAR(`started_at`) AS year_n, MONTH(`started_at`) AS month_n, DAY(`started_at`) AS day_n,
+       ROW_NUMBER() OVER(PARTITION BY year_n, month_n, day_n) AS rank
+      FROM `track` WHERE valid = 1 AND image != '' HAVING year_n = current_year AND month_n = current_month
+    ) ranked
+    WHERE rank <= 8
+  ;
 
 END;
 $$
